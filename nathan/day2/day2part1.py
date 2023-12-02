eed756e35c783d753c0c93eb851f6b96d317a52d
nathan <nshirts7@gmail.com>
2023-12-02 10:11:24 -0800

    finishes part 1

---
+++ nathan/day2/day2part1.py
@@ -0,0 +1,52 @@
+input = open('inputDay2.txt', 'r').read().split('\n')
+limits = {
+    'red': 12,
+    'green': 13,
+    'blue': 14,
+}
+
+
+def createDataStructure(data):
+  structure = []
+
+  for item in data:
+
+    color_list = []
+    scores = item.strip().split(';')
+
+    for score in scores:
+
+      score = score.split(',')
+
+      for groups in score:
+        parts = groups.split()
+        color = parts[-1]
+        number = int(parts[-2])
+        color_list.append([color, number])
+
+    structure.append(color_list)
+  return structure
+
+
+def sumId(list):
+  idList = []
+
+  for id, items in enumerate(list):
+    
+    if cubeLimit(items):
+      idList.append((id + 1))
+
+  return sum(idList)
+
+
+def cubeLimit(list):
+
+  for item in list:
+    if item[1] > limits[item[0]]:
+      return False
+    
+  return True
+
+
+games = createDataStructure(input)
+print('the sum is', sumId(games))
